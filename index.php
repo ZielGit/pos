7805775a8d88b80a25b6079acdc8981ac5c3f1c2
Ziel <frans_421_12@hotmail.com>
2021-08-31 23:29:23 -0500

    feat: Creación de modelos y controladores

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
 require_once "controllers/layout.controller.php";
+require_once "controllers/UserController.php";
+require_once "controllers/CategoryController.php";
+require_once "controllers/ProductController.php";
+require_once "controllers/ClientController.php";
+require_once "controllers/SaleController.php";
+
+require_once "models/User.php";
+require_once "models/Category.php";
+require_once "models/Product.php";
+require_once "models/Client.php";
+require_once "models/Sale.php";
 
 $layout = new ControllerLayout();
 $layout -> ctrLayout();
