9a6147e68555e9148147ca4dc19bb67d768c35d4
Ziel <frans_421_12@hotmail.com>
2021-10-02 23:09:34 -0500

    feat: Se agrego tabla de usuario

--- views/modulos/usuarios.php
+++ views/modulos/usuarios.php
@@ -22,28 +22,71 @@
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Title</h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
+          <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">
+            Agregar Usuario
+          </button>
         </div>
         <div class="card-body">
-          Start creating your amazing application!
+
+          <table class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nombre</th>
+                <th>Usuario</th>
+                <th>Foto</th>
+                <th>Perfil</th>
+                <th>Estado</th>
+                <th>Ultimo Login</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Usuario Administrador</td>
+                <td>admin</td>
+                <td><img src="views/dist/img/avatar5.png" class="img-thumbnail" width="40px"></td>
+                <td>Administrador</td>
+                <td><button class="btn btn-success btn-xs">Activado</button></td>
+                <td>2017-12-11 12:05:32</td>
+                <td>
+                    <button class="btn btn-warning"><i class="fas fa-pencil-alt"></i></button>
+                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
         </div>
         <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
       </div>
       <!-- /.card -->
 
     </section>
     <!-- /.content -->
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="modalAgregarUsuario">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Default Modal</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
