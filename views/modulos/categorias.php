e89ef67fe639f3ede3cbad40c6d1a47a86ef8bca
Ziel <frans_421_12@hotmail.com>
2021-11-20 23:27:36 -0500

    feat: Categoria tabla y modal

--- views/modulos/categorias.php
+++ views/modulos/categorias.php
@@ -22,28 +22,146 @@
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Title</h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
+          <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarCategoria">
+            Agregar Categoría
+          </button>
         </div>
         <div class="card-body">
-          Start creating your amazing application!
+
+          <table id="datatable" class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th style="width: 10px;">#</th>
+                <th>Categoría</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <td>1</td>
+              <td>Fuente de Poder</td>
+              <td>
+                <button class="btn btn-warning btnEditarUsuario" idUsuario="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarUsuario">
+                  <i class="fas fa-pencil-alt"></i>
+                </button>
+
+                <button class="btn btn-danger btnEliminarUsuario" idUsuario="'.$value["id"].'" fotoUsuario="'.$value["foto"].'" usuario="'.$value["usuario"].'"><i class="fa fa-times"></i></button>
+              </td>
+              <!--  
+              <?php 
+                $item = null;
+                $valor = null;
+                $usuarios = UserController::MostrarUsuario($item, $valor);
+
+                foreach ($usuarios as $key => $value) {
+                  echo '<tr>
+                          <td>1</td>
+                          <td>'.$value["nombre"].'</td>
+                          <td>'.$value["usuario"].'</td>';
+                          if ($value["foto"] != "") {
+                            echo '<td><img src="'.$value["foto"].'" class="img-thumbnail" width="40px"></td>';
+                          } else {
+                            echo '<td><img src="views/dist/img/avatar5.png" class="img-thumbnail" width="40px"></td>';
+                          }
+                          echo '<td>'.$value["perfil"].'</td>';
+                          if ($value["estado"] != 0) {
+                            echo '<td><button class="btn btn-success btn-xs btnActivar" idUsuario="'.$value["id"].'" estadoUsuario="0">Activado</button></td>';
+                          } else {
+                            echo '<td><button class="btn btn-danger btn-xs btnActivar" idUsuario="'.$value["id"].'" estadoUsuario="1">Desactivado</button></td>';
+                          }
+                          
+                          echo '<td>'.$value["ultimo_login"].'</td>
+                          <td>
+                              <button class="btn btn-warning btnEditarUsuario" idUsuario="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarUsuario">
+                                <i class="fas fa-pencil-alt"></i>
+                              </button>
+
+                              <button class="btn btn-danger btnEliminarUsuario" idUsuario="'.$value["id"].'" fotoUsuario="'.$value["foto"].'" usuario="'.$value["usuario"].'"><i class="fa fa-times"></i></button>
+                          </td>
+                        </tr>';
+                }
+              ?> -->
+            </tbody>
+          </table>
+
         </div>
         <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
       </div>
       <!-- /.card -->
 
     </section>
     <!-- /.content -->
 </div>
+
+<!-- Modal Agregar Categoria -->
+<div class="modal fade" id="modalAgregarCategoria">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form action="" role="form" method="post">
+        <div class="modal-header">
+          <h4 class="modal-title">Agregar Categoría</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fa fa-tags"></i></span>
+            </div>
+            <input type="text" class="form-control input-log" id="nuevaCategoria" name="nuevaCategoria" placeholder="Ingresar Categoría" required>
+          </div>
+
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Salir</button>
+          <button type="submit" class="btn btn-primary">Guardar Categoria</button>
+        </div>
+        <!-- <?php
+          $crearUsuario = new UserController();
+          $crearUsuario->CrearUsuario();
+        ?> -->
+      </form>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.Modal Agregar Categoria -->
+
+<!-- Modal Editar Categoria -->
+<div class="modal fade" id="modalEditarCategoria">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form role="form" method="post">
+        <div class="modal-header">
+          <h4 class="modal-title">Editar Categoría</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fa fa-user"></i></span>
+            </div>
+            <input type="text" class="form-control input-log" id="editarCategoria" name="editarCategoria" value="" required>
+          </div>
+
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Salir</button>
+          <button type="submit" class="btn btn-primary">Editar Categoría</button>
+        </div>
+        <!-- <?php
+          $editarUsuario = new UserController();
+          $editarUsuario->EditarUsuario();
+        ?> -->
+      </form>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.Modal Editar Categoria -->
