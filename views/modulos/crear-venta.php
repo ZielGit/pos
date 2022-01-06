f9edf6f8c17d155add44d2bdea63449b9e1b3ad7
Ziel <frans_421_12@hotmail.com>
2022-01-05 20:38:08 -0500

    feat: Diseño Venta

--- views/modulos/crear-venta.php
+++ views/modulos/crear-venta.php
@@ -6,44 +6,226 @@
                 <div class="col-sm-6">
                     <h1>Crear Venta</h1>
                 </div>
-                <!-- <div class="col-sm-6">
+                <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Blank Page</li>
+                      <li class="breadcrumb-item"><a href="inicio">Inicio</a></li>
+                      <li class="breadcrumb-item active">Crear Venta</li>
                     </ol>
-                </div> -->
+                </div>
             </div>
         </div><!-- /.container-fluid -->
     </section>
 
     <!-- Main content -->
     <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12 col-xl-5">
+            <div class="card card-success card-outline">
+              <div class="card-header">
+              </div>
+              <form action="" method="post">
+                <div class="card-body pad table-responsive">
+                  
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="fa fa-user"></i></span>
+                    </div>
+                    <input type="text" class="form-control input-log" id="editarNombre" name="editarNombre" value="" required>
+                  </div>
+                  
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="fa fa-code"></i></span>
+                    </div>
+                    <input type="text" class="form-control input-log" id="nuevoCodigo" name="nuevoCodigo" placeholder="Ingresar Código" readonly required>
+                  </div>
 
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Title</h3>
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="fas fa-users"></i></span>
+                    </div>
+                    <select class="form-control input-lg" name="seleccionarCliente" id="seleccionarCliente" required>
+                      <option value="">Selecionar Cliente</option>
+                      <?php
+                        $item = null;
+                        $valor = null;
+                        $cliente = ClientController::MostrarClientes($item, $valor);
 
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
+                        foreach ($cliente as $key => $value) {
+                          echo '<option value="'.$value["id"].'">'.$value["nombre"].'</option>';
+                        }
+                      ?>
+                    </select>
+                    <span class="input-group-append">
+                      <button type="button" class="btn btn-primary btn-flat" data-toggle="modal" data-target="#modalAgregarCliente">
+                        Agregar Cliente
+                      </button>
+                    </span>
+                  </div>
+
+                  <div class="form-group row nuevo Producto">
+
+                  </div>
+                  <input type="hidden" id="" name="">
+
+                  <button type="button" class="btn btn-info d-lg-none btnAgregarProducto">Agregar producto</button>
+
+                  <div class="row">
+                    <div class="col-md-8 ml-auto float-right">
+                      <table class="table">
+                        <thead>
+                          <th>Impuesto</th>
+                          <th>Total</th>
+                        </thead>
+                        <tbody>
+                          <td>
+                            <div class="input-group">
+                              <input type="number" class="form-control" placeholder="0" required>
+                              <div class="input-group-append">
+                                <span class="input-group-text">
+                                  <i class="fas fa-percent"></i>
+                                </span>
+                              </div>
+                            </div>
+                          </td>
+                          <td>
+                            <div class="input-group">
+                              <input type="text" class="form-control" placeholder="00000" required>
+                              <div class="input-group-append">
+                                <span class="input-group-text">
+                                  <i class="fas fa-dollar-sign"></i>
+                                </span>
+                              </div>
+                            </div>
+                          </td>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <div class="input-group mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fas fa-users"></i></span>
+                      </div>
+                      <select class="form-control input-lg" name="nuevoMetodoPago" id="nuevoMetodoPago" required>
+                        <option value="">Selecione método de pago</option>
+                        <option value="Efectivo">Efectivo</option>
+                        <option value="TC">Tarjeta de crédito</option>
+                        <option value="TD">Tarjeta de Débito</option>
+                      </select>
+                    </div>
+
+                    <div class="cajasMetodoPago"></div>
+                    <input type="hidden" id="listaMetodoPago" name="listaMetodoPago">
+                  </div>
+
+                </div>
+                <!-- /.card -->
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary ml-auto float-right">Guardar Venta</button>
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
-        <div class="card-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
+          <!-- /.col -->
 
+          <div class="col-sm-12 col-xl-7">
+            <div class="card card-warning card-outline">
+              <div class="card-header">
+              </div>
+              <div class="card-body pad table-responsive">
+                <table class="table table-bordered table-striped">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Imagen</th>
+                      <th>Código</th>
+                      <th>Descripción</th>
+                      <th>Stock</th>
+                      <th>Acciones</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- ./row -->
+      </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
 </div>
+
+<!-- Modal Agregar Cliente -->
+<div class="modal fade" id="modalAgregarCliente" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form action="" role="form" method="post">
+        <div class="modal-header">
+          <h4 class="modal-title">Agregar Cliente</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fas fa-user"></i></span>
+            </div>
+            <input type="text" class="form-control input-log" name="nuevoCliente" placeholder="Ingresar nombre" required>
+          </div>
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fas fa-id-card"></i></span>
+            </div>
+            <input type="number" class="form-control input-log" name="nuevoDocumentoId" placeholder="Ingresar documento" required>
+          </div>
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+            </div>
+            <input type="email" class="form-control input-log" name="nuevoEmail" placeholder="Ingresar email" required>
+          </div>
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fas fa-phone"></i></span>
+            </div>
+            <input type="text" class="form-control" name="nuevoTelefono" data-inputmask="'mask': ['999-999-999 [x99999]', '+099 999 999[9]-999']" data-mask placeholder="Ingresar telefono" required>
+          </div>
+          
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fas fa-map-marker-alt"></i></span>
+            </div>
+            <input type="text" class="form-control input-log" name="nuevaDireccion" placeholder="Ingresar dirección" required>
+          </div>
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+            </div>
+            <input type="text" class="form-control" name="nuevaFechaNacimiento" data-inputmask-alias="datetime" data-inputmask-inputformat="yyyy/mm/dd" data-mask placeholder="Ingresar fecha nacimiento" required>
+          </div>
+
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Salir</button>
+          <button type="submit" class="btn btn-primary">Guardar Cliente</button>
+        </div>
+        <?php
+          $crearCliente = new ClientController();
+          $crearCliente->CrearCliente();
+        ?>
+      </form>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.Modal Agregar Cliente -->
--- views/modulos/ventas.php
+++ views/modulos/ventas.php
@@ -6,12 +6,12 @@
                 <div class="col-sm-6">
                     <h1>Administrar Ventas</h1>
                 </div>
-                <!-- <div class="col-sm-6">
+                <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Blank Page</li>
+                      <li class="breadcrumb-item"><a href="inicio">Inicio</a></li>
+                      <li class="breadcrumb-item active">Administrar Ventas</li>
                     </ol>
-                </div> -->
+                </div>
             </div>
         </div><!-- /.container-fluid -->
     </section>
@@ -22,25 +22,60 @@
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Title</h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
+          <a href="crear-venta">
+            <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarVenta">
+              Agregar Venta
             </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
+          </a>
         </div>
         <div class="card-body">
-          Start creating your amazing application!
+          <table id="datatable" class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th style="width: 10px;">#</th>
+                <th>Código factura</th>
+                <th>Cliente</th>
+                <th>Vendedor</th>
+                <th>Forma de Pago</th>
+                <th>Neto</th>
+                <th>Total</th>
+                <th>Fecha</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- <?php
+                $item = null;
+                $valor = null;
+                //$ventas = SaleController::MostrarVentas($item, $valor);
+
+                foreach ($Ventas as $key => $value) {
+                  echo '<tr>
+                          <td>'.$value["id"].'</td>
+                          <td>'.$value["nombre"].'</td>
+                          <td>'.$value["documento"].'</td>
+                          <td>'.$value["email"].'</td>
+                          <td>'.$value["telefono"].'</td>
+                          <td>'.$value["direccion"].'</td>
+                          <td>'.$value["fecha_nacimiento"].'</td>             
+                          <td>'.$value["compras"].'</td>
+                          <td>0000-00-00 00:00:00</td>
+                          <td>'.$value["fecha"].'</td>
+                          <td>
+                            <button class="btn btn-warning btnEditarVenta" data-toggle="modal" data-target="#modalEditarVenta" idVenta="'.$value["id"].'">
+                              <i class="fas fa-pencil-alt"></i>
+                            </button>
+
+                            <button class="btn btn-danger btnEliminarVenta" idVenta="'.$value["id"].'"><i class="fa fa-times"></i></button>
+                          </td>
+                        </tr>';
+                }
+              ?> -->
+            </tbody>
+          </table>
+
         </div>
         <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
       </div>
       <!-- /.card -->
 
