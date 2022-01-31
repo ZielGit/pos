4f88bfffc8f393d44237a6f8d6372f3b4101d88c
Ziel <frans_421_12@hotmail.com>
2022-01-30 23:47:44 -0500

    Se cambio el grafico de morris js por chart js

--- views/modulos/reportes/grafico_ventas.php
+++ views/modulos/reportes/grafico_ventas.php
@@ -31,46 +31,91 @@
 ?>
 
 <!-- Grafico de ventas -->
-<div class="card bg-gradient-default">
-    <div class="card-header">
+<div class="card bg-gradient-info">
+    <div class="card-header border-0">
         <h3 class="card-title">
-            <i class="fas fa-th mr-1"></i> Grafico de Ventas
+            <i class="fas fa-th mr-1"></i>
+            Grafico de Ventas
         </h3>
     </div>
     <div class="card-body">
-        <div class="chart" id="line-chart-ventas" style="height: 250px;"></div>
+        <canvas class="chart" id="line-chart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
     </div>
+    <!-- /.card-body -->
 </div>
+<!-- /.card -->
 
 <script>
-var line = new Morris.Line({
-    element          : 'line-chart-ventas',
-    resize           : true,
-    data             : [
-        <?php
-            if ($noRepetirFechas != null){
+    // Sales graph chart
+    var salesGraphChartCanvas = $('#line-chart').get(0).getContext('2d')
+
+    var salesGraphChartData = {
+        labels: [
+            <?php
                 foreach($noRepetirFechas as $key){
-                    echo "{ y: '".$key."', ventas: ".$sumaPagosMes[$key]." },";
+                    echo '"'.$key.'",';
                 }
-                echo "{y: '".$key."', ventas: ".$sumaPagosMes[$key]." }";
-            } else {
-            echo "{ y: '0', ventas: '0' }";
+            ?>
+        ],
+        datasets: [
+            {
+                label: 'Ventas',
+                fill: false,
+                borderWidth: 2,
+                lineTension: 0,
+                spanGaps: true,
+                borderColor: '#efefef',
+                pointRadius: 3,
+                pointHoverRadius: 7,
+                pointColor: '#efefef',
+                pointBackgroundColor: '#efefef',
+                data: [
+                    <?php
+                        foreach($noRepetirFechas as $key){
+                            echo "".$sumaPagosMes[$key].",";
+                        }
+                    ?>
+                ]
             }
-        ?>
-    ],
-    xkey             : 'y',
-    ykeys            : ['ventas'],
-    labels           : ['ventas'],
-    lineColors       : ['#065AA6'],
-    lineWidth        : 2,
-    hideHover        : 'auto',
-    gridTextColor    : '#03203B',
-    gridStrokeWidth  : 0.4,
-    pointSize        : 4,
-    pointStrokeColors: ['#065AA6'],
-    gridLineColor    : '#03203B',
-    gridTextFamily   : 'Open Sans',
-    preUnits         : '$',
-    gridTextSize     : 10
-});
+        ]
+    }
+
+    var salesGraphChartOptions = {
+        maintainAspectRatio: false,
+        responsive: true,
+        legend: {
+            display: false
+        },
+        scales: {
+            xAxes: [{
+            ticks: {
+                fontColor: '#efefef'
+            },
+            gridLines: {
+                display: false,
+                color: '#efefef',
+                drawBorder: false
+            }
+            }],
+            yAxes: [{
+            ticks: {
+                stepSize: 5000,
+                fontColor: '#efefef'
+            },
+            gridLines: {
+                display: true,
+                color: '#efefef',
+                drawBorder: false
+            }
+            }]
+        }
+    }
+
+    // This will get the first returned node in the jQuery collection.
+    // eslint-disable-next-line no-unused-vars
+    var salesGraphChart = new Chart(salesGraphChartCanvas, { // lgtm[js/unused-local-variable]
+        type: 'line',
+        data: salesGraphChartData,
+        options: salesGraphChartOptions
+    })
 </script>
