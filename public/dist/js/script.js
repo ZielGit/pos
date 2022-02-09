e6f0f48f009b7082bfe9898cfaadd195e581a89d
Ziel <frans_421_12@hotmail.com>
2022-02-09 15:55:09 -0500

    Se restauro pdfmake y jszip, se arreglo las tablas

--- public/dist/js/script.js
+++ public/dist/js/script.js
@@ -1,5 +1,5 @@
 // DataTables
 $("#datatable").DataTable({
-    "responsive": true, "lengthChange": false, "autoWidth": false,
-    "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-}).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    "responsive": true, "lengthChange": true, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+}).buttons().container().appendTo('#datatable_wrapper .col-md-6:eq(0)');
