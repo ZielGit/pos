aefc00d9106ea0c11ea17f56096de85302ba00f8
Frans <frans_421_12@hotmail.com>
2023-10-04 22:41:23 -0500

    install jquery

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -30,7 +30,7 @@
 
 		<!-- Plugins -->
 		<!-- jQuery -->
-		<script src="public/plugins/jquery/jquery.min.js"></script>
+        <script src="../node_modules/jquery/dist/jquery.min.js"></script>
 		<!-- Bootstrap 4 -->
 		<script src="public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 		<!-- Select2 -->
