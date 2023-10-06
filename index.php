65d3aff897ee577885f3ea3b88473e86a44fc445
Frans <frans_421_12@hotmail.com>
2023-10-05 20:55:52 -0500

    install phpdotenv

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'vendor/autoload.php';
+
 require_once "controllers/LayoutController.php";
 require_once "controllers/UserController.php";
 require_once "controllers/CategoryController.php";
@@ -12,6 +14,9 @@ require_once "models/Category.php";
 require_once "models/Product.php";
 require_once "models/Client.php";
 require_once "models/Sale.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $layout = new LayoutController();
 $layout->Layout();
